c45460f5776fef6fad4c16ac68d7e1da56c23d4e
MuhammadUmair98 <MuhammadUmaira98@gmail.com>
2023-08-29 12:04:38 +0100

    Design changes

--- resources/js/Pages/customerViews/customerPricing.vue
+++ resources/js/Pages/customerViews/customerPricing.vue
@@ -1,64 +1,63 @@
 <template>
     <div>
-        <Head title="Dashboard"><title>Pricing</title></Head>
-        <h1 class="mb-8 text-3xl font-bold">Pricing</h1>
-
-        <div class="bg-white py-16">
-            <div class="container mx-auto text-center">
-                <h2 class="text-3xl font-semibold mb-6">Pricing</h2>
-                <p class="text-lg text-gray-600 mb-8">
-                    Choose a plan that fits your needs
-                </p>
-                <div
-                    class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"
-                >
-                    <div
-                        v-for="plan in plans"
-                        :class="{
-                            'border border-green-500':
-                                !!activePrice &&
-                                !!plan.stripe_plan &&
-                                activePrice === plan.stripe_plan,
-                        }"
-                        class="p-4 bg-gray-100 rounded-lg shadow"
-                    >
-                        <h3 class="text-xl font-semibold mb-2">
-                            {{ plan.name }}
-                        </h3>
-                        <p class="text-gray-600 mb-4">
-                            {{ plan.description }}
-                        </p>
-                        <p class="text-2xl font-semibold mb-2">
-                            ${{ plan.price }}/mo
-                        </p>
-                        <ul class="text-left text-gray-600 mb-4">
-                            <li v-for="option in plan.options">{{ option }}</li>
-                        </ul>
-                        <Link
-                            :class="
-                                !!activePrice &&
-                                !!plan.stripe_plan &&
-                                activePrice === plan.stripe_plan
-                                    ? 'bg-green-500 hover:bg-green-800'
-                                    : 'bg-rgb-primary hover:bg-gray-800'
-                            "
-                            class="text-white px-4 py-2 rounded-full text-lg transition duration-300 ease-in-out"
-                            :href="'/customer/checkout/' + plan.slug"
-                        >
-                            {{
-                                !!activePrice &&
-                                !!plan.stripe_plan &&
-                                activePrice === plan.stripe_plan
-                                    ? "Subscribed"
-                                    : "Select Plan"
-                            }}
-                        </Link>
-                    </div>
-                </div>
+      <Head title="Pricing" />
+      <h1 class="mb-8 text-3xl font-bold">Pricing</h1>
+  
+      <div class="bg-gradient-to-b from-blue-800 via-blue-900 to-gray-900 min-h-screen">
+        <div class="container mx-auto text-center py-16">
+          <h2 class="text-3xl font-semibold mb-6 text-white">Pricing</h2>
+          <p class="text-lg text-gray-400 mb-8">
+            Choose a plan that fits your needs
+          </p>
+          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div
+              v-for="plan in plans"
+              :class="{
+                'border border-green-500':
+                  !!activePrice &&
+                  !!plan.stripe_plan &&
+                  activePrice === plan.stripe_plan,
+              }"
+              class="p-4 bg-gray-100 rounded-lg shadow transform hover:-translate-y-1 transition-transform"
+            >
+              <h3 class="text-xl font-semibold mb-2">{{ plan.name }}</h3>
+              <p class="text-gray-600 mb-4">{{ plan.description }}</p>
+              <p class="text-2xl font-semibold mb-2">${{ plan.price }}/mo</p>
+              <ul class="text-left text-gray-600 mb-4">
+                <li v-for="option in plan.options">{{ option }}</li>
+              </ul>
+              <Link
+                :class="
+                  !!activePrice &&
+                  !!plan.stripe_plan &&
+                  activePrice === plan.stripe_plan
+                    ? 'bg-green-500 hover:bg-green-800'
+                    : 'bg-rgb-primary hover:bg-gray-800'
+                "
+                class="text-white px-4 py-2 rounded-full text-lg transition duration-300 ease-in-out"
+                :href="'/customer/checkout/' + plan.slug"
+              >
+                {{
+                  !!activePrice &&
+                  !!plan.stripe_plan &&
+                  activePrice === plan.stripe_plan
+                    ? 'Subscribed'
+                    : 'Select Plan'
+                }}
+              </Link>
             </div>
+          </div>
         </div>
+      </div>
+  
+      <footer class="bg-gray-800 py-8 mt-auto">
+        <div class="container mx-auto text-center">
+          <p class="text-gray-400">&copy; 2023 ezepost. All rights reserved.</p>
+        </div>
+      </footer>
     </div>
-</template>
+  </template>
+  
 
 <script>
 import { Head, Link } from "@inertiajs/vue3";
--- resources/js/Pages/guestpages/Pricing.vue
+++ resources/js/Pages/guestpages/Pricing.vue
@@ -1,92 +1,134 @@
 <template>
-    <Head title="Pricing" />
     <div>
+      <Head title="Pricing" />
+      <div class="flex flex-col min-h-screen">
         <guest-nav></guest-nav>
+  
+         <div class="flex-grow bg-gradient-to-b from-blue-800 via-blue-900 to-gray-900 py-16">
+        <div class="container mx-auto text-center">
+          <h2 class="text-4xl font-semibold text-white mb-6">Pricing</h2>
+          <p class="text-lg text-white mb-8">
+            Choose a plan that fits your needs
+          </p>
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+            <!-- Basic Plan -->
+            <div class="p-6 bg-gray-800 rounded-lg shadow-lg text-white ">
 
-        <!-- Pricing Section -->
-        <div class="bg-white py-16">
-            <div class="container mx-auto text-center">
-                <h2 class="text-3xl font-semibold mb-6">Pricing</h2>
-                <p class="text-lg text-gray-600 mb-8">
-                    Choose a plan that fits your needs
-                </p>
-                <div
-                    class="flex flex-col md:flex-row justify-center space-y-6 md:space-y-0 md:space-x-6"
+              <div class="bg-blue-600 rounded-full p-4 mb-4 inline-block animate-bounce">
+                <svg
+                  class="w-12 h-12 mx-auto text-white"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                    <!-- Basic Plan -->
-                    <div
-                        class="w-full md:w-1/3 lg:w-1/4 p-4 bg-gray-100 rounded-lg shadow"
-                    >
-                        <h3 class="text-xl font-semibold mb-2">Basic</h3>
-                        <p class="text-gray-600 mb-4">
-                            Perfect for individuals
-                        </p>
-                        <p class="text-2xl font-semibold mb-2">$9.99/mo</p>
-                        <ul class="text-left text-gray-600 mb-4">
-                            <li>10 GB Storage</li>
-                            <li>Unlimited Transfers</li>
-                            <li>Email Support</li>
-                        </ul>
-                        <button
-                            class="bg-rgb-primary text-white px-4 py-2 rounded-full text-lg hover:bg-gray-800 transition duration-300 ease-in-out"
-                        >
-                            Select Plan
-                        </button>
-                    </div>
-                    <!-- Pro Plan -->
-                    <div
-                        class="w-full md:w-1/3 lg:w-1/4 p-4 bg-gray-100 rounded-lg shadow"
-                    >
-                        <h3 class="text-xl font-semibold mb-2">Pro</h3>
-                        <p class="text-gray-600 mb-4">
-                            For businesses and teams
-                        </p>
-                        <p class="text-2xl font-semibold mb-2">$19.99/mo</p>
-                        <ul class="text-left text-gray-600 mb-4">
-                            <li>50 GB Storage</li>
-                            <li>Unlimited Transfers</li>
-                            <li>Priority Support</li>
-                        </ul>
-                        <button
-                            class="bg-rgb-primary text-white px-4 py-2 rounded-full text-lg hover:bg-gray-800 transition duration-300 ease-in-out"
-                        >
-                            Select Plan
-                        </button>
-                    </div>
-                    <!-- Enterprise Plan -->
-                    <div
-                        class="w-full md:w-1/3 lg:w-1/4 p-4 bg-gray-100 rounded-lg shadow"
-                    >
-                        <h3 class="text-xl font-semibold mb-2">Enterprise</h3>
-                        <p class="text-gray-600 mb-4">
-                            For large organizations
-                        </p>
-                        <p class="text-2xl font-semibold mb-2">Contact Us</p>
-                        <ul class="text-left text-gray-600 mb-4">
-                            <li>Customized Solutions</li>
-                            <li>Unlimited Transfers</li>
-                            <li>Dedicated Support</li>
-                        </ul>
-                        <button
-                            class="bg-rgb-primary text-white px-4 py-2 rounded-full text-lg hover:bg-gray-800 transition duration-300 ease-in-out"
-                        >
-                            Contact Us
-                        </button>
-                    </div>
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M5 13l4 4L19 7"
+                  ></path>
+                </svg>
+              </div>
+              <h3 class="text-xl font-semibold mb-4">Basic</h3>
+              <p class="text-whte-300 mb-6">Perfect for individuals</p>
+              <p class="text-2xl font-semibold text-blue-400 mb-6">$9.99/mo</p>
+              <ul class="text-left text-white-300 mb-6 space-y-2">
+                <li>10 GB Storage</li>
+                <li>Unlimited Transfers</li>
+                <li>Email Support</li>
+              </ul>
+              <button
+                class="bg-rgb-primary text-white px-6 py-3 rounded-full text-lg hover:bg-gray-700 hover:text-blue-400 transition duration-300 ease-in-out transform hover:scale-105"
+              >
+                Select Plan
+              </button>
+            </div>
+              <!-- Pro Plan -->
+              <div class="p-6 bg-gray-800 rounded-lg shadow-lg text-white ">
+
+                <div class="bg-blue-600 rounded-full p-4 mb-4 inline-block animate-bounce">
+                  <svg
+                    class="w-12 h-12 mx-auto text-white"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M13 10V3L4 14h7v7l9-11h-7z"
+                    ></path>
+                  </svg>
                 </div>
+                <h3 class="text-xl font-semibold mb-4">Pro</h3>
+                <p class="text-white-300 mb-6">For businesses and teams</p>
+                <p class="text-2xl font-semibold text-blue-400 mb-6">$19.99/mo</p>
+                <ul class="text-left text-white-300 mb-6 space-y-2">
+                  <li>50 GB Storage</li>
+                  <li>Unlimited Transfers</li>
+                  <li>Priority Support</li>
+                </ul>
+                <button
+                  class="bg-rgb-primary text-white px-6 py-3 rounded-full text-lg hover:bg-gray-700 hover:text-blue-400 transition duration-300 ease-in-out transform hover:scale-105"
+                >
+                  Select Plan
+                </button>
+              </div>
+              <!-- Enterprise Plan -->
+              <div class="p-6 bg-gray-800 rounded-lg shadow-lg text-white ">
+
+                <div class="bg-blue-600 rounded-full p-4 mb-4 inline-block animate-bounce">
+                  <svg
+                    class="w-12 h-12 mx-auto text-white"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                    ></path>
+                  </svg>
+                </div>
+                <h3 class="text-xl font-semibold mb-4">Enterprise</h3>
+                <p class="text-white-300 mb-6">For large organizations</p>
+                <p class="text-2xl font-semibold text-blue-400 mb-6">Contact Us</p>
+                <ul class="text-left text-white-300 mb-6 space-y-2">
+                  <li>Customized Solutions</li>
+                  <li>Unlimited Transfers</li>
+                  <li>Dedicated Support</li>
+                </ul>
+                <button
+                  class="bg-rgb-primary text-white px-6 py-3 rounded-full text-lg hover:bg-gray-700 hover:text-blue-400 transition duration-300 ease-in-out transform hover:scale-105"
+                >
+                  Contact Us
+                </button>
+              </div>
             </div>
+          </div>
         </div>
-        <footer style="margin-top: 4rem" class="bg-rgb-primary py-8">
-            <div class="container mx-auto text-center">
-                <p class="text-gray-300">
-                    &copy; 2023 ezepost. All rights reserved.
-                </p>
-            </div>
-        </footer>
+      </div>
+      <footer class="bg-gray-800 py-8">
+        <div class="container mx-auto text-center">
+          <p class="text-gray-400">
+            &copy; 2023 ezepost. All rights reserved.
+          </p>
+        </div>
+      </footer>
     </div>
-</template>
+  </template>
+  
+  
+  
 
 <script>
+
 import GuestNav from "../../shared/GuestNav.vue";
 import { Head } from "@inertiajs/vue3";
 export default {
